7f392e568db4bafde2b14b202b9c4aedcc632da8
franciley45 <testepa45@gmail.com>
2023-06-06 17:13:14 -0300

    feat: configured to use vitest+RTL

--- Front-End/vite.config.js
+++ Front-End/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setupTests.js',
+    css: true,
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+    },
+    reporters: ['verbose'],
+  },
 })
