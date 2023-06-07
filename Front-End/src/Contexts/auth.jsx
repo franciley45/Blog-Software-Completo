dc8ee4fe6ffa2d5f4baafa450e0772ac4f28b205
franciley45 <testepa45@gmail.com>
2023-06-07 17:52:48 -0300

    feat: localStorage is enter is exit button logic

--- Front-End/src/Contexts/auth.jsx
+++ Front-End/src/Contexts/auth.jsx
@@ -3,23 +3,27 @@ import { useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext();
 
-export const AuthProvider = ({children}) => {
+export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const navigate = useNavigate();
 
-  const login = (email, password) => {
-    console.log('login', { email, password });
-    if(password === "secret"){
-        setUser({ id: '123', email });
-        navigate('/')
-    }
-    
-  }
-  const logout = () => {
-    console.log("logout");
-    setUser(null)
-  };
-    return(
+    const login = (email, password) => {
+        const loggedUser = { id: "123", email };
+
+        localStorage.setItem("user", JSON.stringify(loggedUser))
+
+        if (password === "secret") {
+            setUser(loggedUser);
+            navigate('/');
+        }
+
+    };
+    const logout = () => {
+        console.log("logout");
+        localStorage.removeItem("user");
+        setUser(null);
+    };
+    return (
         <AuthContext.Provider value={{ authenticated: !!user, user, login, logout }}>{children}</AuthContext.Provider>
     )
 };
--- Front-End/src/Pages/Home.jsx
+++ Front-End/src/Pages/Home.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from "react-router-dom";
 function Home() {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null)
+  useEffect(() => {
+    const result = localStorage.getItem("user")
+    setUser(result)
+  })
+  const handleClick = () => {
+    navigate('/login')
+  }
+  const handleClick2 = () => {
+    localStorage.removeItem("user")
+    setUser(null)
+  }
   return (
+    <>
     <div>Home</div>
+   
+    {user ? <button type='button' onClick={handleClick2}>Sair</button> : 
+     <button type='button' onClick={handleClick}>Login</button>
+    }
+    
+    </>
+    
   )
 }
 
