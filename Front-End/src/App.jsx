0f548e61a2c8f9af2d67291441a7fc8c41d878c8
franciley45 <testepa45@gmail.com>
2023-06-07 10:55:45 -0300

    feat: created Home component

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Login from './Pages/Login'
+import Home from './Pages/Home'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
     <>
       <h1>APP</h1>
       <Login/>
+      <Home/>
       
     </>
   )
