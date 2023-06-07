85ca1d61616ed60d1ee84fdd8adb12ae60387e7d
franciley45 <testepa45@gmail.com>
2023-06-07 16:43:12 -0300

    feat: navigation

--- Front-End/src/Contexts/auth.jsx
+++ Front-End/src/Contexts/auth.jsx
@@ -1,18 +1,25 @@
 import React, { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({children}) => {
-    const [user, setUser] = useState(null)
+    const [user, setUser] = useState(null);
+    const navigate = useNavigate();
 
   const login = (email, password) => {
-    console.log('login', { email, password })
-    setUser({ id: '123', email })
+    console.log('login', { email, password });
+    if(password === "secret"){
+        setUser({ id: '123', email });
+        navigate('/')
+    }
+    
   }
   const logout = () => {
-    console.log("logout")
-  }
+    console.log("logout");
+    setUser(null)
+  };
     return(
         <AuthContext.Provider value={{ authenticated: !!user, user, login, logout }}>{children}</AuthContext.Provider>
     )
-}
+};
