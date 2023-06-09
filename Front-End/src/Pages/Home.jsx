bf6bc1e7df64c0ffa1e80c23c7560777c1ca3bdf
franciley45 <testepa45@gmail.com>
2023-06-09 12:52:47 -0300

    feat: Home page style

--- Front-End/src/Pages/Home.jsx
+++ Front-End/src/Pages/Home.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useContext, useState } from 'react'
 import { AuthContext } from '../Contexts/auth'
-import { getAllDate } from '../Services/Api';
+import { getAllDate } from '../Services/Api'
+import "../Style/HomeStyle.css"
 
 function Home() {
-  const[posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([])
   const { authenticated, setUser, logout, clickLogin } = useContext(AuthContext);
-
-  console.log(posts)
 
   useEffect(() => {
     const storage = localStorage.getItem("user");
@@ -15,14 +14,56 @@ function Home() {
       const response = await getAllDate();
       setPosts(response.data);
     })();
-  },[])
+  }, [])
 
+  function getRandomInt() {
+    const random = String(Math.floor(Math.random() * (4 - 1) + 1))
+    const result = `../../src/Imagens/b${random}.jpg`
+    return result
+  }
   return (
     <>
-      <div>Home</div>
-      {authenticated ? <button type='button' onClick={logout}>Sair</button> :
-        <button type='button' onClick={clickLogin}>Login</button>
-      }
+
+      <div id="blog">
+        <div class="blog-heading">
+          <h3>TESTEMUNHOS DE FÉ – BLOG</h3>
+        </div>
+        <div>
+            {authenticated ? <button type='button'className='btn' onClick={logout}>Sair</button> :
+              <button type='button' className='btn' onClick={clickLogin}>Login</button>
+            }
+          </div>
+      </div>
+      {posts.map((post) =>
+        <section id="blog" key={post.posts_id}>
+          <div class="blog-box-container">
+
+            <div class="blog-box">
+              <div class="blog-box-img">
+                <img src={getRandomInt()} alt="blog" />
+                <a href="#" class="blog-img-link">
+                  <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                </a>
+              </div>
+
+              <div class="blog-box-text">
+                <strong>Artificial Intelligence</strong>
+                <a href="#">{post.title}</a>
+                <p>{post.milagres}</p>
+                <div class="blog-author">
+                  <div class="blog-author-img">
+                    <img src={getRandomInt()} alt="" />
+                  </div>
+                  <div class="blog-author-text">
+                    <strong>Alax</strong>
+                    <span>{post.date}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      )}
     </>
   )
 }
