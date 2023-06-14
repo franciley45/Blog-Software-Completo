1968e65b24f754009927d6a972c72fff2745c721
franciley45 <testepa45@gmail.com>
2023-06-14 17:31:11 -0300

    feat: configuring .env to be able to communicate with Railway

--- Back-End/src/models/db/connection.js
+++ Back-End/src/models/db/connection.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE  || 'EspiritoSanto',
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE  || 'EspiritoSanto',
 });
 
 module.exports = connection;
