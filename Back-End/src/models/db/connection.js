82c73bc0d042c3db4a541b6f3f752ce72716a1c4
franciley45 <testepa45@gmail.com>
2023-06-15 16:23:05 -0300

    feat: changes in connection

--- Back-End/src/models/db/connection.js
+++ Back-End/src/models/db/connection.js
@@ -3,10 +3,12 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const connection = mysql.createPool({
-  host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE  || 'EspiritoSanto',
+  database: process.env.MYSQLDATABASE,
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+
 });
 
 module.exports = connection;
