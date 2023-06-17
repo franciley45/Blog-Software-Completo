a677c833437a5e33236ad4562164138bd3d68752
franciley45 <testepa45@gmail.com>
2023-06-16 23:04:04 -0300

    ajuste

--- Back-End/src/models/db/connection.js
+++ Back-End/src/models/db/connection.js
@@ -2,6 +2,7 @@ const mysql = require('mysql2/promise');
 
 require('dotenv').config();
 
+
 const connection = mysql.createPool({
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
