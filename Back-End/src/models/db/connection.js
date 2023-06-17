ac1d44bdb760f570bdeb50c164073c16119a5696
franciley45 <testepa45@gmail.com>
2023-06-17 16:16:33 -0300

    poll

--- Back-End/src/models/db/connection.js
+++ Back-End/src/models/db/connection.js
@@ -3,13 +3,12 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 
-const connection = mysql.createPool({
+const connection = mysql.createPool({ 
+  host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  host: process.env.MYSQLHOST,
-  port: process.env.MYSQLPORT,
-
-});
+}, 
+{ pooling: { enabled: true, maxIdleTime: 10000, maxSize: 45, queueTimeout: 60000 } });
 
 module.exports = connection;
