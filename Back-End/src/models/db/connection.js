72a4a7b4b3af6966e46543bf7fc13dd74f778b45
franciley45 <testepa45@gmail.com>
2023-10-01 17:37:22 -0300

    connect/postgreSQL

--- Back-End/src/models/db/connection.js
+++ Back-End/src/models/db/connection.js
@@ -1,11 +1,23 @@
-const mysql = require('mysql2/promise');
-
+const { Pool } = require('pg')
 require('dotenv').config();
 
 async function connect() {
-  const connection = await mysql.createConnection(process.env.DATABASE);
-  return connection
+  const pool = new Pool({
+    connectionString: process.env.DATABASEPOSTGRESQL
+  })
+  const client = await pool.connect()
+  client.release()
+  return client
 }
 
 module.exports = connect;
+
+//banco MYSQL
+/* const mysql = require('mysql2/promise');
+async function connect() {
+  const connection = await mysql.createConnection(process.env.DATABASEMYSQL);
+  return connection
+}
+
+module.exports = connect; */
 
