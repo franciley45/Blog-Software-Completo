00ed268cbe467ec4a042dda7cef2b6203546f772
franciley45 <testepa45@gmail.com>
2023-10-20 02:32:29 -0300

    ...

--- Back-End/src/app.js
+++ Back-End/src/app.js
@@ -3,28 +3,28 @@ const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
-const { SwaggerUIBundle, SwaggerUIStandalonePreset } = require('swagger-ui-dist');
 const loginRouter = require('./routes/loginRoute');
 const milagresRouter = require('./routes/milagresRoute');
 const userRouter = require('./routes/userRoute');
 
 const app = express();
 
- async function testFetch() {
+ /* async function testFetch() {
     const res = await fetch('https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css');
     const options = {
-        customCssUrl: [res]
+        customCssUrl: res,
         };
     return options
-}
+} */
 
-  
+var options = {
+    customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css'
+  };
 app.use(express.json());
 app.use(cors()) // Use this after the variable declaration
 app.use('/post', milagresRouter)
 app.use('/user', userRouter)
 app.use('/login', loginRouter)
-app.use('/api-docs', swaggerUi.serveFiles(swaggerDocument, testFetch()));
-app.get('/api-docs', swaggerUi.setup(swaggerDocument, testFetch()));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 
 module.exports = app;
