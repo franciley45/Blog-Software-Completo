625cee573f0330f15d0529e66b48fd6b5b9c482c
franciley45 <testepa45@gmail.com>
2023-10-20 01:42:47 -0300

    ...

--- Back-End/src/app.js
+++ Back-End/src/app.js
@@ -9,20 +9,20 @@ const userRouter = require('./routes/userRoute');
 
 const app = express();
 
- /* async function testFetch() {
+ async function testFetch() {
     const res = await fetch('https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css');
     const options = {
         customCssUrl: res,
         };
     return options
-} */
+}
 
 app.use(express.json());
 app.use(cors()) // Use this after the variable declaration
 app.use('/post', milagresRouter)
 app.use('/user', userRouter)
 app.use('/login', loginRouter)
-app.use('/api-docs', swaggerUi.serve);
-app.get('/api-docs', swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serveFiles(swaggerDocument, testFetch()));
+app.get('/api-docs', swaggerUi.setup(swaggerDocument, testFetch()));
 
 module.exports = app;
